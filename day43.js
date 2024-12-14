e5d2b6d39787f129a1208d665324d988913dd612
RahulMirji <121561908+RahulMirji@users.noreply.github.com>
2024-12-14 20:35:09 +0530

    day 43 added

---
+++ day43.js
@@ -0,0 +1,12 @@
+// Strings
+let name = "Rahul"
+console.log(typeof name);
+
+//Numbers
+let age = 20;
+console.log(typeof age);
+
+// Booleans
+let isStudent = true;
+console.log(typeof isStudent)
+
