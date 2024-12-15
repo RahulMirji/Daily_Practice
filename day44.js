326ddaf9c613caef59657f3d31a8d668522596b3
RahulMirji <121561908+RahulMirji@users.noreply.github.com>
2024-12-15 23:30:24 +0530

    Day 44 added

---
+++ day44.js
@@ -0,0 +1,15 @@
+// Sum and Difference
+let sum = 5 + 3; // 8
+let difference = 10 - 4; // 6
+console.log(sum, difference);
+
+// Product and quotient
+let product = 6 * 7; // 42
+let quotient = 20 / 4; // 5
+console.log(product, quotient);
+
+// Remainder
+let remainder = 10 % 3; // 1
+console.log(remainder);
+
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+let remainder = 10 % 3; // 1
+// console.log(remainder);
