b7bb66c0947b47fe6ea530daf1cae3b142512acc
RahulMirji <121561908+RahulMirji@users.noreply.github.com>
2024-12-18 17:12:33 +0530

    day 47 added

---
+++ day47.js
@@ -0,0 +1,7 @@
+let fruits = ["apple", "banana", "cherry"];
+console.log(fruits);
+//["apple", "banana", "cherry", "orange"]
+
+fruits.push("orange"); // ["apple", "banana", "cherry", "orange"]
+
+fruits.pop(); // ["apple", "banana", "cherry"]
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,4 +1,1 @@
-for (let i = 1; i <= 100; i++) {  
-//     console.log(i);   
-//   }
- 
+mango
