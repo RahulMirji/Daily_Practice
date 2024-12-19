6cdf129ece565725ba481f8adff7bc2bb169465f
RahulMirji <121561908+RahulMirji@users.noreply.github.com>
2024-12-19 20:56:00 +0530

    Day 48 added

---
+++ day48.js
@@ -0,0 +1,9 @@
+let person = {  
+    name: "John",  
+    age: 30  
+  };  
+  
+  // Accessing properties  
+  console.log(person.name); // Using dot notation  
+  console.log(person["age"]); // Using bracket notation  
+  
