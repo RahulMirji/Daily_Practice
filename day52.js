a3808e99b19b756cb63071ed69c1e036d9742fdd
RahulMirji <121561908+RahulMirji@users.noreply.github.com>
2024-12-24 00:04:08 +0530

    day 52 added

---
+++ day52.js
@@ -0,0 +1,8 @@
+let name = ['Rahul', 'Ganesh', 'Keshav','Sharth', 'giri'];
+name.push("Sumith"); // adding element at the end
+console.log(name)
+
+name.pop(); // for removing last element
+name.shift(); // for removing the first element
+name.unshift("Rahul M");// adding element at the first position
+console.log(name) // printing out put
