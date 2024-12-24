f521f03c65be10f789c4e6bc67a94c2a9a794449
RahulMirji <121561908+RahulMirji@users.noreply.github.com>
2024-12-24 17:06:52 +0530

    day 53 added

---
+++ day53.js
@@ -0,0 +1,12 @@
+// Select the button using its ID
+const button = document.getElementById("myButton");
+
+// Add a click event listener
+button.addEventListener("click", () => {
+  console.log("Button clicked!");
+});
+
+// Add a mouseover event listener
+button.addEventListener("mouseover", () => {
+  console.log("Mouse is over the button!");
+});
